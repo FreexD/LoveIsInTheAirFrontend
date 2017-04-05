6a637f6ef547d5bb979881116f707154a43a358d
Michał Wolny <michalwoln@gmail.com>
2017-04-05 11:22:36 +0200

    initial commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+enablePlugins(ScalaJSPlugin)
+
+name := "LoveIsInTheAirFrontend"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+// This is an application with a main method
+scalaJSUseMainModuleInitializer := true
---
+++ src/main/scala/pl/mwolny/love/frontend/FrontendApp.scala
@@ -0,0 +1,13 @@
+package pl.mwolny.love.frontend
+
+/**
+  * Created by mw on 05/04/17.
+  */
+
+import scala.scalajs.js.JSApp
+
+object FrontendApp extends JSApp {
+  def main(): Unit = {
+    println("Hello world!")
+  }
+}
