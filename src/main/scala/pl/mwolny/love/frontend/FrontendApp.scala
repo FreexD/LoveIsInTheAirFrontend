99cecc75178aeccc965868370188e14cde17368f
Michał Wolny <michalwoln@gmail.com>
2017-05-04 04:55:44 +0200

    add server rest and websocket communication, add first visualisation

--- src/main/scala/pl/mwolny/love/frontend/FrontendApp.scala
+++ src/main/scala/pl/mwolny/love/frontend/FrontendApp.scala
@@ -4,10 +4,44 @@ package pl.mwolny.love.frontend
   * Created by mw on 05/04/17.
   */
 
-import scala.scalajs.js.JSApp
+import scala.scalajs.js.{JSApp}
+import scala.scalajs.js.annotation.JSExportTopLevel
+import org.scalajs.jquery.jQuery
+import fr.hmil.roshttp.HttpRequest
+import monix.execution.Scheduler.Implicits.global
+import fr.hmil.roshttp.Method.POST
+import fr.hmil.roshttp.Method.DELETE
+import fr.hmil.roshttp.Protocol.HTTP
 
 object FrontendApp extends JSApp {
   def main(): Unit = {
-    println("Hello world!")
+    jQuery(() => setupUI())
+  }
+
+  def setupUI(): Unit = {
+    jQuery("#addCreatureButton").click(() => addCreature())
+    jQuery("#killAllCreaturesButton").click(() => killAllCreatures())
+  }
+
+  @JSExportTopLevel("addCreature")
+  def addCreature(): Unit = {
+    HttpRequest()
+        .withProtocol(HTTP)
+        .withHost("localhost")
+        .withPort(9000)
+        .withPath("/creature")
+        .withMethod(POST)
+        .send()
+  }
+
+  @JSExportTopLevel("killAllCreatures")
+  def killAllCreatures(): Unit = {
+    HttpRequest()
+      .withProtocol(HTTP)
+      .withHost("localhost")
+      .withPort(9000)
+      .withPath("/creature")
+      .withMethod(DELETE)
+      .send()
   }
 }
